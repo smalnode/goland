ae30571dbd4a8478fb8de1fe80e30f024f19e648
smalnode <smalnode@gmail.com>
2018-06-03 21:07:03 +0800

    optimize signal

--- lang/signal/main.go
+++ lang/signal/main.go
@@ -11,22 +11,20 @@ func main() {
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT, syscall.SIGKILL)
 
-	for {
-		select {
-		case <-ch:
-			fmt.Println("received syscall.SIGKILL ")
-			os.Exit(0)
-		default:
-			scanMultiply()
-		}
-	}
+	go scanMultiply()
+
+	<-ch
+	fmt.Println("received syscall.SIGKILL ")
+	os.Exit(0)
 }
 
 func scanMultiply() {
-	fmt.Println("Input two integers(CTRL-C to exit): ")
-	var lhs, rhs int64
-	_, err := fmt.Scanf("%d%d\n", &lhs, &rhs)
-	if err == nil {
-		fmt.Println(lhs * rhs)
+	for {
+		fmt.Println("Input two integers(CTRL-C to exit): ")
+		var lhs, rhs int64
+		_, err := fmt.Scanf("%d%d\n", &lhs, &rhs)
+		if err == nil {
+			fmt.Println(lhs * rhs)
+		}
 	}
 }
